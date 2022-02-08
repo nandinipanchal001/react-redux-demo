3f77c152dd59e68fae3b2a4a8c99902a20d2906d
nandinipanchal001 <nandini_panchal@apprely.com>
2022-02-08 14:03:59 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 import React from 'react'
 import Assignment1 from './Assignment1';
 import Assignment2 from './Assignment2';
-import BasicForm from './Assignment3';
-import Assignment4 from './Assignment4';
+import BasicForm from './Assignment4';
 import Assignment5 from './Assignment5';
+import Assignment6 from './Assignment6';
 
 
 const App = () => {
@@ -13,9 +13,9 @@ const App = () => {
     <Switch>
       <Route path={"/assignment-static"}><Assignment1/></Route>
       <Route path={"/assignment-show-my-name"}><Assignment2/></Route>
-      <Route path={"/assignment3"}><BasicForm/></Route>
-      <Route path={"/assignment4"}><Assignment4/></Route>
+      <Route path={"/assignment4"}><BasicForm/></Route>
       <Route path={"/assignment5"}><Assignment5/></Route>
+      <Route path={"/assignment6"}><Assignment6/></Route>
     </Switch>
     </Router>
   );
